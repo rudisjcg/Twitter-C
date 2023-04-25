cea5da5fb6af162173d684e641329269d295d7bf
Rudis <82242977+krakatua@users.noreply.github.com>
2023-04-25 10:14:10 -0400

    SideBar style 60%

---
+++ components/Postfeed.js
@@ -0,0 +1,7 @@
+export default function PostFeed () {
+    return (
+        <div>
+            postFed
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Sidebar from '@/components/Sidebar'
+import PostFeed from '@/components/Postfeed'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <div className='bg-black min-h-screen text-[#e7e9ea] 
+    max-w-[1400] mx-auto'>
+      <Sidebar/>
+      <PostFeed />
+      {/*
       
-    </>
+      <Trending /> */}
+    </div>
   )
 }
