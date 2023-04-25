27c9d6de695914380539d9ecc5c3487038e647cf
Rudis <82242977+krakatua@users.noreply.github.com>
2023-04-25 15:59:23 -0400

    Feed 60% done

---
+++ components/Tweet.js
@@ -0,0 +1,35 @@
+import { ChartBarIcon, ChatIcon, HeartIcon, UploadIcon } from "@heroicons/react/outline"
+
+export default function Tweet() {
+    return (
+        <div className="border-b border-gray-700">
+            <TweetHeader />
+            <div className="p-3 ml-16 text-gray-500 flex space-x-14">
+                <ChatIcon className="w-5 hover:text-green-400"/>
+                <HeartIcon className="w-5 hover:text-pink-400"/>
+                <ChartBarIcon className="w-5 hover:text-green-400 cursor-not-allowed"/>
+                <UploadIcon className="w-5 hover:text-green-400 cursor-not-allowed" />
+            </div>
+        </div>
+    )
+}
+
+
+export function TweetHeader() {
+    return (
+        <div className="flex space-x-3 p-3  border-gray-700">
+            <img
+                className="w-11 h-11 rounded-full 
+            object-cover"
+                src="/assests/kylie.png" />
+                <div>
+                    <div className="text-gray-500 flex space-x-2 items-center mb-1">
+                        <span>@kylie</span>
+                        <div className="w-1 h-1 bg-gray-500 rounded-full"></div>
+                        <span>2 hours ago</span>
+                    </div>
+                    <span>Coemntando miegda</span>
+                </div>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,21 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Sidebar from '@/components/Sidebar'
 import PostFeed from '@/components/Postfeed'
+import Trending from '@/components/Trending'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div className='bg-black min-h-screen text-[#e7e9ea] 
-    max-w-[1400] mx-auto'>
+    max-w-[1400] mx-auto
+    flex '>
       <Sidebar/>
       <PostFeed />
+      <Trending />
       {/*
       
-      <Trending /> */}
+       */}
     </div>
   )
 }
